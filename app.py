60a9b5ca06c1f4314ef221289f32e7353b40abe4
Aryan Panwar <72555591+ARYANPANWAR893@users.noreply.github.com>
2025-11-06 22:39:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1724,4 +1724,5 @@ def reporter_evenements_non_valides():
     conn.close()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
